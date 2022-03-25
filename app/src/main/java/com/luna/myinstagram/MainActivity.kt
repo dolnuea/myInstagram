70a07f4213e1a3f73312764453267821fde1f243
Luna Dagci <ldagci@albany.edu>
2022-03-25 16:29:32 -0400

    Parse Data Objects and Querying

--- app/src/main/java/com/luna/myinstagram/MainActivity.kt
+++ app/src/main/java/com/luna/myinstagram/MainActivity.kt
@@ -2,11 +2,43 @@ package com.luna.myinstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 // let user create a post by taking a photo with their camera
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        queryPosts()
+    }
+
+    // query for all posts in our server 14:45
+    private fun queryPosts() {
+        //specify which class to query
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    Log.e(TAG, "Error fetching posts")
+                }
+                else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post: " + post.getDescription() + " , username: " + post.getUser()?.username)
+                        }
+                    }
+                }
+            }
+
+        })
+    }
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/luna/myinstagram/ParstagramApplication.kt
+++ app/src/main/java/com/luna/myinstagram/ParstagramApplication.kt
@@ -2,11 +2,16 @@ package com.luna.myinstagram
 
 import android.app.Application
 import com.parse.Parse;
+import com.parse.ParseObject
 
 class ParstagramApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post::class.java)
+        // Add your initialization code here
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
